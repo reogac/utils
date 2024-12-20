1eb3a01d40b0c8c58dd8f2f1866888a71f795095
Thai Quang Tung <tqtung@gmail.com>
2024-12-20 22:00:22 +0900

    first commit

---
+++ error-wrapper.go
@@ -0,0 +1,31 @@
+package utils
+
+import "fmt"
+
+type errorWrapper struct {
+	msg  string
+	prev error
+}
+
+func WrapError(msg string, prev error) error {
+	return &errorWrapper{
+		msg:  msg,
+		prev: prev,
+	}
+}
+
+func (wErr *errorWrapper) Error() (errStr string) {
+	errStr = wErr.msg
+
+	prev := wErr.prev
+	for prev != nil {
+		if cur, ok := prev.(*errorWrapper); ok {
+			errStr = fmt.Sprintf("%s <<= %s", errStr, cur.msg)
+			prev = cur.prev
+		} else {
+			errStr = fmt.Sprintf("%s <<= %s", errStr, prev.Error())
+			prev = nil
+		}
+	}
+	return
+}
